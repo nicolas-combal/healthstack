fb8877dce5da24949939bc84169f24e346dcd32f
Alconis-github <ncombal@gmail.com>
2025-06-19 14:47:04 +0200

    Changed front page and added hot reloading for the front

--- app-front/src/App.tsx
+++ app-front/src/App.tsx
@@ -1,79 +1,15 @@
-import React, { useEffect, useState } from "react";
-
-type ServiceStatus = "online" | "offline" | "checking";
-
-interface Service {
-  name: string;
-  url: string;
-}
-
-const services: Service[] = [
-    { name: "Gateway", url: "/health" },
-    { name: "Service Auth", url: "/auth" },
-    { name: "Service Reports", url: "/reports" },
-];
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
 const App: React.FC = () => {
-  const [status, setStatus] = useState<Record<string, ServiceStatus>>({});
-
-  const checkServiceStatus = async () => {
-    const newStatus: Record<string, ServiceStatus> = {};
-
-    await Promise.all(
-        services.map(async (service) => {
-          newStatus[service.name] = "checking";
-          try {
-            const response = await fetch(service.url, { method: "GET" });
-            newStatus[service.name] =
-                response.ok ? "online" : "offline";
-          } catch (error) {
-            newStatus[service.name] = "offline";
-          }
-        })
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </Router>
     );
-
-    setStatus(newStatus);
-  };
-
-  useEffect(() => {
-    checkServiceStatus();
-    const interval = setInterval(checkServiceStatus, 5000); // vérifie toutes les 5s
-    return () => clearInterval(interval);
-  }, []);
-
-  const getStatusColor = (s: ServiceStatus) => {
-    switch (s) {
-      case "online":
-        return "green";
-      case "offline":
-        return "red";
-      default:
-        return "gray";
-    }
-  };
-
-  return (
-      <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-        <h1>État des microservices</h1>
-        <ul>
-          {services.map((service) => (
-              <li key={service.name} style={{ margin: "1rem 0" }}>
-                  <a href={`http://localhost:8000${service.url}`}
-                     rel="noopener noreferrer"> {service.name}
-                  </a>:{" "}
-                  <span
-                    style={{
-                      color: getStatusColor(status[service.name]),
-                      fontWeight: "bold",
-                    }}
-                >
-              {status[service.name] ?? "checking..."}
-            </span>
-              </li>
-          ))}
-        </ul>
-      </div>
-  );
 };
 
 export default App;
