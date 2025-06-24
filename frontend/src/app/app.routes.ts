6be46491a2d1913fc6918566762e4de4859cf044
Alconis-github <ncombal@gmail.com>
2025-06-24 11:43:13 +0200

    Updated angular frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
+import {Home} from "./pages/home/home";
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+    {path: '', component: Home}
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class App {
+  protected title = 'frontend';
 }
